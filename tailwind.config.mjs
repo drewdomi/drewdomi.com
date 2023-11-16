74e4f854d2892990b20ad49bf1ab751e4046a820
Andrew Domingues <drewdomi@proton.me>
2023-11-16 16:37:21 -0300

    feat: setup dark & light custom theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,8 +7,32 @@ export default {
 	plugins: [require('daisyui')],
 	daisyui: {
 		themes: [
-			'light',
-			'dark'
+			{
+				dark: {
+					...require("daisyui/src/theming/themes")['dark'],
+					'primary': '#005AAD',
+					'seconday': '#CA1D1D',
+					'accent': '#0E1337',
+					'neutral': '#59C8FF',
+					"neutral-content": "#EFEFEF",
+					"base-100": "#2B2B2B",
+					"base-200": "#2B2B2B",
+					"base-300": "#2B2B2B",
+					"base-content": "#EFEFEF",
+				},
+				light: {
+					...require("daisyui/src/theming/themes")['light'],
+					'primary': '#005AAD',
+					'seconday': '#CA1D1D',
+					'accent': '#0E1337',
+					'neutral': '#59C8FF',
+					"neutral-content": "#2B2B2B",
+					"base-100": "#EFEFEF",
+					"base-200": "#EFEFEF",
+					"base-300": "#EFEFEF",
+					"base-content": "#2B2B2B",
+				},
+			}
 		]
 	}
 }

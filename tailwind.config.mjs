d884327e815986a6fb6e94e759d82233fd5ee390
Andrew Domingues <drewdomi@proton.me>
2023-11-17 10:39:17 -0300

    feat: about section mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: 'Roboto',
+			title: 'Rubik',
+			body: 'Roboto'
+		},
 		extend: {},
 	},
 	plugins: [require('daisyui')],

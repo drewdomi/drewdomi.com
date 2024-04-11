40a2853d8dfd2babfa89d89457023dac168c6663
Andrew Domingues <drewdomi@proton.me>
2024-04-11 15:14:25 -0300

    fix: footer color

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'DrewDomi';
+export const SITE_TITLE = 'Drew Domi';
 export const SITE_DESCRIPTION = 'Andrew Domingues portfólio';
 
 export const LINKS = {

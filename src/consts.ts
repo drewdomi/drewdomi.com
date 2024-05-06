135be07f10f1596ccbb19bc1e73e6c758ed38b3c
Andrew Domingues <drewdomi@proton.me>
2024-05-06 16:32:13 -0300

    style: add prettier

--- src/consts.ts
+++ src/consts.ts
@@ -1,12 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Drew Domi';
-export const SITE_DESCRIPTION = 'Andrew Domingues portfólio';
+export const SITE_TITLE = 'Drew Domi'
+export const SITE_DESCRIPTION = 'Andrew Domingues portfólio'
 
 export const LINKS = {
-  about: "/",
-  projects: "/projects",
-  blog: "/blog",
-  contato: "/#contact",
-};
+  about: '/',
+  projects: '/projects',
+  blog: '/blog',
+  contato: '/#contact',
+}

a6fc7e785780eeb574eb68375bbc92b352243dcb
Andrew Domingues <contato@drewdomi.com>
2024-10-05 11:46:56 -0300

    chore: setup inlang i18n for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,29 @@
 import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import paraglide from '@inlang/paraglide-astro'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
-import sitemap from '@astrojs/sitemap'
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind(), mdx(), sitemap()],
   site: 'https://drewdomi.com',
+  domains: {
+    en: 'https://drewdomi.com',
+    'pt-br': 'https://drewdomi.com',
+  },
+  i18n: {
+    locales: ['en', 'pt-br'],
+    defaultLocale: 'en',
+  },
+  integrations: [
+    paraglide({
+      project: './project.inlang',
+      outdir: './src/paraglide',
+    }),
+    icon(),
+    tailwind(),
+    mdx(),
+    sitemap(),
+  ],
 })

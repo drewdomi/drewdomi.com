4b88fc9792679847c358fd601d57186dedbb1e8e
Andrew Domingues <contact@drewdomi.com>
2025-02-14 21:16:51 -0300

    feat: updates feb 2025

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,32 @@
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import paraglide from '@inlang/paraglide-astro'
-import icon from 'astro-icon'
-import { defineConfig } from 'astro/config'
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import paraglide from "@inlang/paraglide-astro";
+import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
-import vercel from '@astrojs/vercel/serverless'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://drewdomi.com',
-  output: 'server',
+  site: "https://drewdomi.com",
+  output: "server",
   server: {
     headers: {
-      'Last-Modified': 'Fri, 11 Oct 2024 18:08:35 GMT',
+      "Last-Modified": "Fri, 11 Oct 2024 18:08:35 GMT",
     },
   },
   i18n: {
-    locales: ['en', 'pt-br'],
-    defaultLocale: 'en',
+    locales: ["en", "pt-br"],
+    defaultLocale: "en",
     routing: {
       prefixDefaultLocale: true,
     },
   },
   integrations: [
     paraglide({
-      project: './project.inlang',
-      outdir: './src/paraglide',
+      project: "./project.inlang",
+      outdir: "./src/paraglide",
     }),
     icon(),
     tailwind(),
@@ -34,4 +34,4 @@ export default defineConfig({
     sitemap(),
   ],
   adapter: vercel(),
-})
+});

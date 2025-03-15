ecd8e0d7314f82c81e6637ab3ccdcf89dc4de495
Andrew Domingues <contact@drewdomi.com>
2025-03-15 13:04:36 -0300

    fix: Layout.astro format on prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,26 +1,33 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ['prettier-plugin-astro'],
   overrides: [
     {
-      files: "*.astro",
+      files: '*.astro',
       options: {
-        parser: "astro",
+        parser: 'astro',
       },
     },
     {
-      files: "*.html",
+      files: 'src/layouts/Layout.astro', // Target only this specific file
       options: {
-        parser: "angular",
+        parser: 'astro',
+        singleAttributePerLine: false, // Override for this file only
+      },
+    },
+    {
+      files: '*.html',
+      options: {
+        parser: 'angular',
       },
     },
   ],
   tabWidth: 2,
   bracketSpacing: true,
-  endOfLine: "lf",
+  endOfLine: 'lf',
   singleQuote: true,
   semi: false,
   singleAttributePerLine: true,
-  arrowParens: "avoid",
-};
+  arrowParens: 'avoid',
+}

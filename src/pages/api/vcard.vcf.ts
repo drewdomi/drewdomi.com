e356951cdb0bd14d3325fa3cc24ddcd429970ba7
Andrew Domingues <contact@drewdomi.com>
2026-04-29 20:03:12 +0000

    feat: add vcard

---
+++ src/pages/api/vcard.vcf.ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from 'astro'
+import vcardContent from './assets/drewdomi_vcard.vcf?raw'
+
+export const GET: APIRoute = async () => {
+  return new Response(vcardContent, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/vcard',
+      'Content-Disposition': 'attachment; filename="drewdomi_vcard.vcf"',
+      'Cache-Control': 'no-store',
+    },
+  })
+}
